bd9081765be696353f145e9cdc7c427f4a8dc4bb
Vexx <advikdivekar@gmail.com>
2026-03-05 16:31:17 +0530

    faster response test v1

--- api/click.go
+++ api/click.go
@@ -1,24 +1,18 @@
-package handler
-
-import (
-	"database/sql"
-	"net/http"
-	"os"
-
-	_ "github.com/lib/pq" // Postgres driver
-)
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 	dbURL := os.Getenv("DATABASE_URL")
 
-	// Connect to Neon DB
-	db, err := sql.Open("postgres", dbURL)
-	if err == nil {
-		defer db.Close()
-		// Increment the click count silently
-		db.Exec("UPDATE melt_stats SET clicks = clicks + 1 WHERE id = 1")
+	// We update the DB, but we don't want to make the user wait forever
+	if dbURL != "" {
+		db, err := sql.Open("postgres", dbURL)
+		if err == nil {
+			defer db.Close()
+			// Optimization: Execute and don't spend time scanning results
+			_, _ = db.Exec("UPDATE melt_stats SET clicks = clicks + 1 WHERE id = 1")
+		}
 	}
 
-	// Instantly redirect back to your GitHub profile
+	// Redirect to your profile immediately
+	// Using 302 (Found) instead of 301 ensures browsers don't cache the redirect itself
+	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 	http.Redirect(w, r, "https://github.com/advikdivekar", http.StatusFound)
 }
